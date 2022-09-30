28057f362e44f8f38cdaf1b6e9a46cb20916dfe1
Juan M. Flaherty <juanmanuelflaherty@gmail.com>
2022-09-29 21:36:02 -0300

    + Ported project to NodeJS to enable development tools + Added http-serve to support local automated testing + Added Playwright and a basic test in TypeScript

---
+++ tests/game.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+let palabras = ["verdugo", "caballero", "espada", "laud", "cruzada"];
+
+test("Play new game", async ({ page }) => {
+  const newGameButton = page.locator(".btn-iniciarjuego");
+  const inGameNewGameButton = page.locator(".btn-nuevojuego");
+  const loseAlert = page.locator("#alerta-muere");
+  const winAlert = page.locator("#alerta-vive");
+
+  await page.goto("/");
+
+  await newGameButton.click();
+
+  for (let word of palabras) {
+    for (let character of word.split("")) {
+      await page.keyboard.press(character);
+      if (await loseAlert.isVisible()) {
+        await inGameNewGameButton.click();
+        break;
+      }
+    }
+    if (await winAlert.isVisible()) {
+      break;
+    }
+  }
+  await expect(winAlert).toBeVisible();
+});
