f3dd32543a9647e5abe64a91bce70ac8dccc8481
Juan M. Flaherty <juanmanuelflaherty@gmail.com>
2022-09-29 23:07:51 -0300

    + Added simple README.md + Added .nvmrc file for NVM to pickup the correct NodeJS version + Fixed test logic so it keeps retrying with random words until it wins

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
+# Ahorcado + Playwright Test
+
 [![Playwright Tests](https://github.com/jmflaherty/LUCASMSALDANA-ONE_Challange_Ahorcado/actions/workflows/playwright.yml/badge.svg)](https://github.com/jmflaherty/LUCASMSALDANA-ONE_Challange_Ahorcado/actions/workflows/playwright.yml)
+
+Sample project based on the [Ahorcado](https://github.com/LUCASMSALDANA/LUCASMSALDANA-ONE_Challange_Ahorcado) by my friend [Lucas Saldaña](https://github.com/LUCASMSALDANA), adding a Playwright test that always wins the game. Eventually.
+
+## Setup
+
+1. Install NodeJs 18.10.0.
+
+   > Recommended to use [NVM](https://github.com/nvm-sh/nvm).
+   >
+   > After installing it, simply enter `nvm use` in the project's root folder.
+
+1. Install the project's dependencies.
+
+   `npm install`
+
+1. Install Playwright's dependencies.
+
+   `sudo npx playwright install-deps`
+
+## How to use
+
+### Serve website
+
+`npm run serve`
+
+### Run test
+
+`npm run test`
+
+### Show test report
+
+`npm run report`
--- tests/game.spec.ts
+++ tests/game.spec.ts
@@ -1,7 +1,6 @@
-import { test, expect } from "@playwright/test";
+import step, { test, expect } from "@playwright/test";
 
 let palabras = ["verdugo", "caballero", "espada", "laud", "cruzada"];
-
 test("Play new game", async ({ page }) => {
   const newGameButton = page.locator(".btn-iniciarjuego");
   const inGameNewGameButton = page.locator("#btn-nuevojuego");
@@ -9,20 +8,24 @@ test("Play new game", async ({ page }) => {
   const winAlert = page.locator("#alerta-vive");
 
   await page.goto("/");
-
   await newGameButton.click();
 
-  for (let word of palabras) {
-    for (let character of word.split("")) {
-      await page.keyboard.press(character);
-      if (await loseAlert.isVisible()) {
-        await inGameNewGameButton.click();
-        break;
-      }
-    }
-    if (await winAlert.isVisible()) {
-      break;
-    }
-  }
+  await test.step("Guesses", async () => {
+    let attempt = 0;
+    do {
+      const word = palabras[Math.floor(Math.random() * palabras.length)];
+      await test.step(`#${attempt} - Guessing with ${word}`, async () => {
+        for (let character of word.split("")) {
+          await page.keyboard.press(character);
+          if (await loseAlert.isVisible()) {
+            await inGameNewGameButton.click();
+            break;
+          }
+        }
+      });
+      ++attempt
+    } while (await winAlert.isHidden());
+  });
+
   await expect(winAlert).toBeVisible();
 });
